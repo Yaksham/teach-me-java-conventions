0bf755dbcec7f5688ad9d5b597c83e1f4ce9b99a
Tejasv Plawat <>
2024-12-25 19:26:30 +0530

    1. rename class names to pascal case 2. add test files to correct package structure 3. use correct assert statements in tests 4. rename methods and variables to be more descriptive

---
+++ src/test/java/powerpackage/PowerTest.java
@@ -0,0 +1,27 @@
+package powerpackage;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PowerTest {
+
+    @Test
+    public void testOneRaisedToOne() {
+        assertEquals(1, PowerFinder.calculatePower(1, 1));
+    }
+
+    @Test
+    public void testTwoRaisedToOne() {
+        assertEquals(2, PowerFinder.calculatePower(2, 1));
+    }
+
+    @Test
+    public void testTwoRaisedToTwo() {
+        assertEquals(4, PowerFinder.calculatePower(2, 2));
+    }
+
+    @Test
+    public void testThreeRaisedToTwo() {
+        assertEquals(9, PowerFinder.calculatePower(3, 2));
+    }
+}
